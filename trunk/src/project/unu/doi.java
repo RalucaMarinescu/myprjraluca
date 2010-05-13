eb5e475ade44a57818cec2ea89b33a5fa538794a
mm.raluca@gmail.com <mm.raluca@gmail.com@9b444606-2b65-c159-6414-3c1006569869>
2010-05-13 16:07:51 +0000

    first5 version

---
+++ trunk/src/project/unu/doi.java
@@ -0,0 +1,13 @@
+package project.unu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class doi extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
